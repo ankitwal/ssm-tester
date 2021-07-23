5719a8794de875bee5793161690008d1ecbe3f9c
Ankit Wal <ankit.wal@gmail.com>
2021-07-23 16:54:10 +0800

    Update the documentation for the test function API

--- tester/tcp_connection_tester.go
+++ tester/tcp_connection_tester.go
@@ -8,10 +8,29 @@ import (
 	"time"
 )
 
+// TcpConnectionTestWithNameTag return true if instances are found and all instance can run connection command successfully.
+// It fails the test if no instances are found to match the Name tag.
+// It fails the test if any one of the instances cannot run the command successfully or within timeout, or any other error.
+// It passes the test if all found instances for tag Name run the command successfully in the given timeouts.
+//
+// tagName should be the string value of the tag:Name of the target EC2 instances.
+// endpoint, and port should be the network endpoint to validate tcp connectivity to.
+//
+// maxRetries specifies the number of times the test should poll AWS API for results of the command sent to the target EC2 VMs.
+// waitBetweenRetires specifies the duration in time.Seconds to wait between each retry.
+// these values may need to be adjusted for the total number of ec2 instances that are expected to run the test command.
+func TcpConnectionTestWithNameTag(t *testing.T, client commandSenderLister, tagName string, endpoint string, port string, maxRetries int, waitBetweenRetries time.Duration) {
+	_, err := TcpConnectionTestWithNameTagE(t, client, tagName, endpoint, port, maxRetries, waitBetweenRetries)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
 // TcpConnectionTestWithNameTagE return true if instances are found and all instance can run connection command successfully.
 // It returns false and an error if no instances are found to match the Name tag.
 // It returns false and an error if any one of the instances cannot run the command successfully or within timeout.
-// It Returns false and error for any other error.
+// It returns false and error for any other error.
+// It returns true and nil error if all found instances for tag Name run the command successfully in the given timeouts.
 func TcpConnectionTestWithNameTagE(t *testing.T, client commandSenderLister, tagName string, endpoint string, port string, maxRetries int, waitBetweenRetries time.Duration) (bool, error) {
 	// build command
 	tcpConnectionTestSendCommandInput := buildSendCommandInputForTcpConnectionWithNameTag(endpoint, port, tagName)
@@ -25,7 +44,8 @@ func buildSendCommandInputForTcpConnectionWithNameTag(endpoint string, port stri
 }
 
 func buildParametersForTcpConnectionWithDefaultTimeout(endpoint string, port string) map[string][]string {
-	const defaultTimeout = "5"
+	// ball park execution timeout that should be okay for testing tcp connectivity
+	const defaultTimeout = "10"
 	command := []string{fmt.Sprintf("bash -c '</dev/tcp/%s/%s'", endpoint, port)}
 	return buildParametersForCommand(command, defaultTimeout)
 }
